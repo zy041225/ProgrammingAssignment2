720f88a07508241569eb7ae3847a9c8fd803e8ab
zy041225 <zy041225@gmail.com>
2015-03-13 21:49:47 +0800

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The functions do store and calculate the matrix and its possible inverse 
 
-## Write a short comment describing this function
+## The mackCacheMatrix stores the matrix or its inverse one by using the set() 
+## setinverse(), it can also get the stored or calculated one by using get() or 
+## getinverse()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setinverse <- function(inverse) {
+    i <<- inverse
+  }
+  getinverse <- function(){
+    i
+  }
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## the cacheSolve check if the inversed matrix is in cache or not and will
+## calculate the inversion when there's no inversed matrix, and also store 
+## the calculating result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+  }
+  else{
+    data <- x$get()
+    i <- solve(data)
+    x$setinverse(i)
+  }
+  i
 }
